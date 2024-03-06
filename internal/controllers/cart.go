88813ccd87ae1ca223ca2a203d7437e14292cfbb
MaksimUlitin <maks_d030p_ulitin@mail.ru>
2024-03-06 20:38:12 +0300

    скелет cart

--- internal/controllers/cart.go
+++ internal/controllers/cart.go
@@ -1,1 +1,28 @@
 package controllers
+
+type Application struct {
+}
+
+func NewApplication() {
+
+}
+
+func CreatingСard() {
+
+}
+
+func DeleteCard() {
+
+}
+
+func GetProductFromCart() {
+
+}
+
+func BuyProductFromCart() {
+
+}
+
+func FastBuy() {
+
+}
