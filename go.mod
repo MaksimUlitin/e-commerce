7b66e7fd12b97c2ff3d4bb09acbd3ae9be26383a
MaksimUlitin <maks_d030p_ulitin@mail.ru>
2024-05-16 18:12:44 +0300

    middleware

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/golang-jwt/jwt v3.2.2+incompatible
 	go.mongodb.org/mongo-driver v1.14.0
 	golang.org/x/crypto v0.19.0
+	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22
 )
 
 require (
@@ -40,5 +41,6 @@ require (
 	golang.org/x/sys v0.17.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	google.golang.org/protobuf v1.30.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/db/cart.go
+++ internal/db/cart.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"log"
+	"time"
 
 	"github.com/maksimulitin/pkg/models"
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -64,6 +65,34 @@ func RemoveCart(ctx context.Context, prodCollection, userCollection *mongo.Colle
 	return nil
 }
 
-func BuyItemFromCart(ctx context.Context, userCollection *mongo.Collection, userId string) error {
-
+func BuyItemFromCart(ctx context.Context, prodCollection, userCollection *mongo.Collection, userId string) error {
+	id, err := primitive.ObjectIDFromHex(userId)
+	if err != nil {
+		log.Println(err)
+		return ErrUserIDIsNotValid
+	}
+	var product_details models.ProductUser
+	var orders_detail models.Order
+	orders_detail.OrderID = primitive.NewObjectID()
+	orders_detail.OrdereredAt = time.Now()
+	orders_detail.OrderCart = make([]models.ProductUser, 0)
+	orders_detail.PaymentMethod.COD = true
+	err = prodCollection.FindOne(ctx, bson.D{primitive.E{Key: "_id", Value: productID}}).Decode(&product_details)
+	if err != nil {
+		log.Println(err)
+	}
+	orders_detail.Price = product_details.Price
+	filter := bson.D{primitive.E{Key: "_id", Value: id}}
+	update := bson.D{{Key: "$push", Value: bson.D{primitive.E{Key: "orders", Value: orders_detail}}}}
+	_, err = userCollection.UpdateOne(ctx, filter, update)
+	if err != nil {
+		log.Println(err)
+	}
+	filter2 := bson.D{primitive.E{key: "_id", Value: id}}
+	update2 := bson.M{"$push": bson.M{"orders.$[].order_list": product_details}}
+	_, err = userCollection.UpdateOne(ctx, filter2, update2)
+	if err != nil {
+		log.Println(err)
+	}
+	return nil
 }
